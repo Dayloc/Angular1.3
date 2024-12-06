afd7f3d2219e2968fe6cadfa4d8d67308730599e
Yumar <daylocyumar@gmail.com>
2024-12-06 16:01:38 +0100

    Ya estamos en DBZ y hemos hecho los componentes

---
+++ src/app/dbz/components/agregar/agregar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar',
+  standalone: false,
+  
+  templateUrl: './agregar.component.html',
+  styleUrl: './agregar.component.css'
+})
+export class AgregarComponent {
+
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzComponent } from './pages/main-dbz.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
 
 
 
 @NgModule({
-  declarations: [DbzComponent],
+  declarations: [DbzComponent, ListadoComponent, AgregarComponent],
   exports: [DbzComponent],  // Make DbzComponent available for other modules to import and use it in their components.
   imports: [
     CommonModule
