c9d40a7720889e007ecd3898fe43b57badc8118e
Yumar <daylocyumar@gmail.com>
2024-12-07 10:35:17 +0100

    Herencia de padre ha hijo

--- src/app/dbz/components/listado/listado.component.ts
+++ src/app/dbz/components/listado/listado.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../../interfaces/personajes.interface';
 
 @Component({
   selector: 'app-listado',
   standalone: false,
-  
+
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
+
+  @Input()
+  public ListaPersonajes:Personaje[]=[{
+    name: 'Spiderman',
+    power: 300
+  },
+  {
+    name: 'Batman',
+    power: 1000
+  },
+  {
+    name: 'Hulk',
+    power: 800
+  },
+  {
+    name: 'Ironman',
+    power: 1500
+  },
+  {
+    name: 'Thor',
+    power: 10000
+  }
+
+
+
+]
 
 }
